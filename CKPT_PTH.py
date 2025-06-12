8932227514a9d6bafdec137d93568d2307373e6b
Nikita <nikitazag3@gmail.com>
2025-06-12 05:13:02 +0000

    Update ckpt_paths

--- CKPT_PTH.py
+++ CKPT_PTH.py
@@ -1,4 +1,7 @@
-LLAVA_CLIP_PATH = None #'/opt/data/private/AIGC_pretrain/LLaVA1.5/clip-vit-large-patch14-336'
-LLAVA_MODEL_PATH = None #'/opt/data/private/AIGC_pretrain/LLaVA1.5/llava-v1.5-13b'
-SDXL_CLIP1_PATH = None #'/opt/data/private/AIGC_pretrain/clip-vit-large-patch14'
-SDXL_CLIP2_CKPT_PTH = None # '/opt/data/private/AIGC_pretrain/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
+LLAVA_CLIP_PATH = (
+    None  #'/opt/data/private/AIGC_pretrain/LLaVA1.5/clip-vit-large-patch14-336'
+)
+LLAVA_MODEL_PATH = "/scratch/imolodetskikh/users/ai_n_zag/SUPIR/weights/llava-v1.5-13b"
+LLAVA_MODEL_PATH = "liuhaotian/llava-v1.5-13b"
+SDXL_CLIP1_PATH = None  #'/opt/data/private/AIGC_pretrain/clip-vit-large-patch14'
+SDXL_CLIP2_CKPT_PTH = None  # '/opt/data/private/AIGC_pretrain/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
